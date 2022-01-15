9d02a5c06672851a2b413a33ce434a73b4423b9d
Lidia <lidia.baciu97>
2022-01-15 22:30:36 +0200

    checking git user change

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ def index():
     conn = get_db_connection()
     users = conn.execute('SELECT * FROM user').fetchall()
     conn.close()
-    print('added some changes here to check git changing username')
     return render_template('index.html', users=users)
 
 @app.route("/user/income/<name>")
