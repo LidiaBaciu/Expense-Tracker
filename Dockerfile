f24675fc5f66174a0396e7f46f33ef52db48a835
Lidia <lbaciu@c-s.ro>
2022-01-15 19:31:57 +0200

    switched to using conda env

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:3.8-alpine
-RUN mkdir /expense_tracker
-ADD . /expense_tracker
-WORKDIR /expense_tracker
-RUN pip install -r requirements.txt
-CMD ["python3", "main.py"]
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+# from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+
+# app.config.from_object(os.environ['APP_SETTINGS'])
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/user/income/<name>")
+def get_income_type(name):
+    return "name: {}".format(name)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
