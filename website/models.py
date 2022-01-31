b344a72395730ae3aaf01385e42ce2ee841f388a
LidiaBaciu <lidia.baciu97@gmail.com>
2022-01-31 20:41:40 +0200

    defined database schema

--- website/models.py
+++ website/models.py
@@ -21,7 +21,6 @@ class Address(db.Model):
     street_no = db.Column(db.Integer)
     city = db.Column(db.String(150))
     country = db.Column(db.String(150))
-    # users = db.relationship('User', secondary=user_address_table)
     
 
 class User(db.Model, UserMixin):
@@ -33,3 +32,57 @@ class User(db.Model, UserMixin):
     created = db.Column(db.DateTime(timezone=True), default=func.now())
     birthday = db.Column(db.DateTime(timezone=True))
     addresses = db.relationship('Address', secondary=user_address_table)
+    incomes = db.relationship('Income')
+    # spendings = db.relationship('Spending')
+    
+
+class IncomeType(db.Model):
+    __tablename__ = 'incomeType'
+    id = db.Column(db.Integer, primary_key=True)
+    income_name = db.Column(db.String(150))
+    
+    
+class Income(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime(timezone=True), default=func.now())
+    amount = db.Column(db.Float)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    income_type_id = db.Column(db.Integer, db.ForeignKey('incomeType.id'))
+   
+
+class SpendingType(db.Model):   
+    __tablename__ = 'spendingType'
+    id = db.Column(db.Integer, primary_key=True)
+    spending_name = db.Column(db.String(150))
+    goal = db.Column(db.Float)
+    
+
+class SpendingCategory(db.Model):
+    __tablename__ = 'spendingCategory'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150))
+    description = db.Column(db.String(150))
+    
+
+class Spending(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime(timezone=True), default=func.now())
+    amount = db.Column(db.Float)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    spending_type_id = db.Column(db.Integer, db.ForeignKey('spendingType.id'))
+    spending_category_id = db.Column(db.Integer, db.ForeignKey('spendingCategory.id'))
+  
+  
+class SavingType(db.Model):   
+    __tablename__ = 'savingType'
+    id = db.Column(db.Integer, primary_key=True)
+    saving_type = db.Column(db.String(150))
+    goal = db.Column(db.Float)
+      
+
+class Saving(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime(timezone=True), default=func.now())
+    amount = db.Column(db.Float)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    saving_type_id = db.Column(db.Integer, db.ForeignKey('savingType.id'))
